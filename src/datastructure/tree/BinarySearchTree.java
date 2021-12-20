08607af4a12c225bc3a708ce33aa95fe4662b9fa
i-am-nemo <sudip313@gmail.com>
2021-12-20 20:27:06 +0530

    BST Traversal

---
+++ src/datastructure/tree/BinarySearchTree.java
@@ -0,0 +1,29 @@
+package src.datastructure.tree;
+
+public class BinarySearchTree {
+
+	Node root;
+
+	BinarySearchTree() {
+		root = null;
+	}
+
+	void insert(int key) {
+		root = insertRec(root, key);
+	}
+
+	Node insertRec(Node root, int key) {
+
+		if (root == null) {
+			root = new Node(key);
+			return root;
+		}
+
+		if (key < root.key)
+			root.left = insertRec(root.left, key);
+		else if (key > root.key)
+			root.right = insertRec(root.right, key);
+
+		return root;
+	}
+}
